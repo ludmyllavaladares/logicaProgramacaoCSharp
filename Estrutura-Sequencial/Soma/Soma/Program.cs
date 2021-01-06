3b9c7d215ea4fef0838fcd52614f8688ed675dbe
Ludmylla Valadares <valadaresludmylla@gmail.com>
2021-01-06 13:59:08 -0300

    Sequencial

---
+++ Estrutura-Sequencial/Soma/Soma/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Soma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, soma;
+            Console.WriteLine("Digite dois números inteiros:");
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+           
+            soma = A + B;
+            Console.WriteLine("A soma é: " + soma);
+        }
+    }
+}
